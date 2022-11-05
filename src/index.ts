a7ef2cb89feb6c4f5f843ce1f36792d8e9a87cd3
Urip Subekti <urip.subekti13@gmail.com>
2022-11-05 22:51:06 +0700

    feat: add uptime

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import v1Routes from "./routes/v1";
-import staticAuth from "./middlewares/static-auth.routes";
 dotenv.config();
 
 const app: Express = express();
@@ -16,7 +15,7 @@ app.get("/", (req: Request, res: Response) => {
   res.send("OK");
 });
 
-app.use("/v1", staticAuth, v1Routes);
+app.use("/v1", v1Routes);
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
